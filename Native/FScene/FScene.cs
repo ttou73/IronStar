3de9246014f51ec7ce0dbca46d8b3955eb724403
demiurghg <demiurghg@gmail.com>
2016-10-31 21:02:15 +0300

    Fixed solution and FScene

---
+++ Native/FScene/FScene.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using Fusion;
+using Fusion.Core.Shell;
+using Fusion.Core.Content;
+using Fusion.Core.IniParser;
+using Fusion.Core.Utils;
+using Native.Fbx;
+using Fusion.Drivers.Graphics;
+using Fusion.Engine.Graphics;
+
+namespace FScene {
+
+	class FScene {
+
+		static int Main ( string[] args )
+		{
+			Thread.CurrentThread.CurrentCulture	=	System.Globalization.CultureInfo.InvariantCulture;
+			Log.AddListener( new StdLogListener() );
+
+			var options = new Options();
+			var parser = new CommandLineParser( options.GetType() );
+
+			try {
+
+				//	parse arguments :
+				parser.ParseCommandLine( options, args );
+
+				//	change extension of output not set :
+				if (options.Output==null) {
+					options.Output = Path.ChangeExtension( options.Input, ".scene");
+				}
+				
+
+				//
+				//	run fbx loader :
+				//
+				Log.Message("Reading FBX: {0}", options.Input);
+
+				var loader = new FbxLoader();
+				using ( var scene  = loader.LoadScene( options.Input, options ) ) {
+				
+					//
+					//	Save scene :
+					//					
+					Log.Message("Preparation...");
+					foreach ( var mesh in scene.Meshes ) {
+						if (mesh!=null) {
+							mesh.MergeVertices( options.MergeTolerance );
+							mesh.DefragmentSubsets(scene, true);
+							mesh.ComputeTangentFrame();
+							mesh.ComputeBoundingBox();
+						}
+					}
+
+					Log.Message("Merging instances...");
+					scene.DetectAndMergeInstances();
+					
+					#if false
+					if (options.BaseDirectory!=null) {
+
+						Log.Message("Resolving assets path...");
+						
+						var relativePath = ContentUtils.MakeRelativePath( options.BaseDirectory + @"\", options.Input );
+						var relativeDir  = Path.GetDirectoryName( relativePath );
+						var sceneDir	 = Path.GetDirectoryName( options.Input );
+						Log.Message("...scene directory      : {0}", sceneDir);
+						Log.Message("...scene base directory : {0}", options.BaseDirectory);
+						Log.Message("...scene relative path  : {0}", relativePath);
+						Log.Message("...scene relative dir   : {0}", relativeDir);
+
+						foreach ( var mtrl in scene.Materials ) {
+							ResolveMaterial( mtrl, relativeDir, sceneDir );
+						}
+					}
+					#endif
+
+					//
+					//	Save scene :
+					//					
+					Log.Message("Writing binary file: {0}", options.Output);
+					using ( var stream = File.OpenWrite( options.Output ) ) {
+						scene.Save( stream );
+					}
+
+
+					if (options.Report) {
+						var reportPath = options.Input + ".html";
+						Log.Message("Writing report: {0}", reportPath);
+						File.WriteAllText( reportPath, SceneReport.CreateHtmlReport(scene));
+					}
+				}
+
+				Log.Message("Done.");
+
+			} catch ( Exception e ) {
+				parser.PrintError( "{0}", e.ToString() );
+
+				if (options.Wait) {
+					Log.Message("Press any key to continue...");
+					Console.ReadKey();
+				}
+
+				return 1;
+			}
+
+			if (options.Wait) {
+				Log.Message("Press any key to continue...");
+				Console.ReadKey();
+			}
+
+			return 0;
+		}
+
+
+
+		#if false
+		static void ResolveMaterial ( MaterialRef material, string relativeSceneDir, string fullSceneDir )
+		{
+			var mtrlName		=	ContentUtils.CreateSafeName( material.Name );
+			var texPath			=	material.Texture ?? "";
+
+			material.Name		=	Path.Combine( relativeSceneDir, mtrlName );
+			material.Texture	=	Path.Combine( relativeSceneDir, texPath );
+			var mtrlFileName	=	Path.Combine( fullSceneDir, mtrlName + ".material" );
+
+
+			if (!File.Exists(mtrlFileName)) {
+
+				var newMtrl =	new BaseIllum();
+
+				newMtrl.ColorTexture.Path		=	ResolveTexture( relativeSceneDir, fullSceneDir, texPath, ""					, newMtrl.ColorTexture.Path		);  
+				newMtrl.SurfaceTexture.Path		=	ResolveTexture( relativeSceneDir, fullSceneDir, texPath, "_surf"			, newMtrl.SurfaceTexture.Path	);  
+				newMtrl.NormalMapTexture.Path	=	ResolveTexture( relativeSceneDir, fullSceneDir, texPath, "_local", "_bump"	, newMtrl.NormalMapTexture.Path	);  
+				newMtrl.EmissionTexture.Path	=	ResolveTexture( relativeSceneDir, fullSceneDir, texPath, "_glow"			, newMtrl.EmissionTexture.Path	);  
+
+				File.WriteAllText( mtrlFileName, BaseIllum.ExportToXml(newMtrl) );
+			}
+
+		}
+
+
+
+		static string ResolveTexture ( string relativeSceneDir, string fullSceneDir, string textureName, string postfix, string fallback )
+		{	
+			if (string.IsNullOrWhiteSpace(textureName)) {
+				return "";
+			}
+
+			var ext		=	Path.GetExtension( textureName );
+			var noExt	=	Path.Combine( Path.GetDirectoryName(textureName), Path.GetFileNameWithoutExtension( textureName ) );
+
+			var fileName	=	Path.Combine( fullSceneDir, noExt + postfix + ext );
+			
+			if ( File.Exists(fileName) ) {
+				return Path.Combine( relativeSceneDir, noExt + postfix + ext );
+			} else {
+				return fallback;
+			}
+		}
+		#endif
+	}
+}
---
+++ Native/FScene/SceneReport.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Core.Mathematics;
+using Fusion.Drivers.Graphics;
+using Fusion.Core;
+using Fusion.Core.Content;
+using Fusion.Engine.Graphics;
+
+namespace FScene {
+	static class SceneReport {
+		/// <summary>
+		/// 
+		/// </summary>
+		static public string CreateHtmlReport ( Scene scene )
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendLine("<pre>");
+
+
+			sb.AppendFormat("<b>Nodes count</b>     : {0}\r\n", scene.Nodes.Count );
+			sb.AppendFormat("<b>Mesh count</b>      : {0}\r\n", scene.Meshes.Count );
+			sb.AppendFormat("<b>Materials count</b> : {0}\r\n", scene.Materials.Count );
+			sb.AppendLine();
+			sb.AppendFormat("<b>Track count</b>     : {0}\r\n", scene.TrackCount );
+			sb.AppendFormat("<b>Frame range</b>     : {0} - {1}\r\n", scene.FirstFrame, scene.LastFrame );
+			sb.AppendFormat("<b>Time range</b>      : {0} - {1}\r\n", scene.StartTime, scene.EndTime );
+
+
+
+
+			sb.AppendLine();
+			sb.AppendLine("<b>Materials:</b>");
+
+			foreach ( var mtrl in scene.Materials ) {
+
+				var index = scene.Materials.IndexOf(mtrl);
+				
+				sb.AppendFormat("{0,4}:  {1,-30} texture:<i>{2}</i>\r\n", index, "\"" + mtrl.Name + "\"", mtrl.Texture );
+			}
+
+
+
+			sb.AppendLine();
+			sb.AppendLine("<b>Meshes:</b>");
+
+			foreach ( var mesh in scene.Meshes ) {
+				
+				var index	=	scene.Meshes.IndexOf(mesh);
+				var verts	=	mesh.VertexCount;
+				var tris	=	mesh.TriangleCount;
+				var subsets	=	mesh.Subsets.Count;
+				var refs	=	string.Join(" ", scene.Nodes.Where( n => n.MeshIndex==index ).Select( n1 => n1.Name ) );
+				var skinned	=	mesh.Vertices.Any( v => v.SkinWeights != Vector4.Zero );
+				
+				sb.AppendFormat("{0,4}:  v:{1,4}  t:{2,4}  s:{3,2}  ref:[<i>{4}</i>] {5}\r\n", index, verts, tris, subsets, refs, skinned ? "skinned":"" );
+			}
+
+
+			sb.AppendLine();
+			sb.AppendLine("<b>Nodes:</b>");
+
+			foreach ( var node in scene.Nodes ) {
+				
+				var index	= scene.Nodes.IndexOf(node);
+				var parent	= node.ParentIndex;
+				var name	= node.Name;
+
+				int depth	=	scene.CalculateNodeDepth(node);
+				var padding	=	new string(' ', depth*2);
+				var hasMesh =	node.MeshIndex >= 0 ? "mesh #" + node.MeshIndex.ToString() : "";
+				
+				sb.AppendFormat("{0,4}:  {1,-30}{2,4}  track:{3}  {4}\r\n", index, padding + name, parent, node.TrackIndex, hasMesh );
+			}
+
+			sb.AppendLine("</pre>");
+
+			return sb.ToString();
+		}
+	}
+}
