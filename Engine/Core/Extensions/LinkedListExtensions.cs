fbeb48235519aa1d3c93fb6fc9d5d40e4585c0cc
demiurghg <demiurghg@gmail.com>
2016-12-17 21:35:26 +0300

    LinkedList RemoveAll extension method

---
+++ Engine/Core/Extensions/LinkedListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusion.Core.Extensions {
+	public static class LinkedListExtensions {
+		public static void RemoveAll<T>( this LinkedList<T> linkedList,
+										Func<T, bool> predicate )
+		{
+			for ( LinkedListNode<T> node = linkedList.First; node != null; ) {
+				LinkedListNode<T> next = node.Next;
+				if ( predicate( node.Value ) )
+					linkedList.Remove( node );
+				node = next;
+			}
+		}
+	}
+}
