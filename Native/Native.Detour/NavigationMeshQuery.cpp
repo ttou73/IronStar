9fee8ee5793f42d7b6c2f11b83c6006ff98c8a93
Vladimir <vvkraf@gmail.com>
2017-02-08 21:18:59 +0300

    DT : add partial queries

--- Native/Native.Detour/NavigationMeshQuery.cpp
+++ Native/Native.Detour/NavigationMeshQuery.cpp
@@ -4,7 +4,8 @@ using namespace Native::Detour;
 
 
 
-OperationStatus Native::Detour::NavigationMeshQuery::FindPath(PolyReference startRef, PolyReference endRef, Vector3 startPos, Vector3 endPos, QueryFilter ^ filter, array<PolyReference>^% path, const int maxPath)
+OperationStatus NavigationMeshQuery::FindPath(PolyReference startRef, PolyReference endRef, Vector3 startPos, Vector3 endPos, 
+											  QueryFilter ^ filter, array<PolyReference>^% path, const int maxPath)
 {
 	float* _sp = new float[3];
 	float* _ep = new float[3];
@@ -15,12 +16,110 @@ OperationStatus Native::Detour::NavigationMeshQuery::FindPath(PolyReference star
 	int pathCount;
 	
 	auto rv = static_cast<OperationStatus>(nativeQuery->findPath(startRef, endRef, _sp, _ep, filter->nativeFilter, _path, &pathCount, maxPath));
-	path = gcnew array<PolyReference>(pathCount);
-	for (int i = 0; i < pathCount; i++) {
-		path[i] = _path[i];
+	if (!Detour::StatusFailed(rv)) {
+		path = gcnew array<PolyReference>(pathCount);
+		for (int i = 0; i < pathCount; i++) {
+			path[i] = _path[i];
+		}
 	}
 	delete _sp;
 	delete _ep;
+	delete _path;
+	return rv;
+}
+
+OperationStatus NavigationMeshQuery::FindStraightPath(Vector3 startPos, Vector3 endPos, array<PolyReference>^ path, int pathSize,
+													 array<Vector3>^% straightPath, array<StraightPathFlags>^% straightPathFlags, 
+													 array<PolyReference>^% straightPathRefs, int maxStraightPath, StraightPathOptions options)
+{
+	float* _sp = new float[3];
+	float* _ep = new float[3];
+	_sp[0] = startPos.X; _sp[1] = startPos.Y; _sp[2] = startPos.X;
+	_ep[0] = endPos.X; _ep[1] = endPos.Y; _ep[2] = endPos.X;
+	pin_ptr<PolyReference> _path = &path[0];
+
+	float* _straightPath = new float[maxStraightPath * 3];
+	uchar* _straightPathFlags = new uchar[maxStraightPath];
+	PolyReference* _straightPathRefs = new PolyReference[maxStraightPath];
+	int pathCount;
+
+	auto rv = static_cast<OperationStatus>(nativeQuery->findStraightPath(_sp, _ep, (PolyReference*)_path, pathSize, _straightPath, _straightPathFlags, _straightPathRefs, &pathCount, maxStraightPath, static_cast<int>(options)));
+	
+	if (!Detour::StatusFailed(rv)) {
+
+		straightPath = gcnew array<Vector3>(pathCount);
+		straightPathFlags = gcnew array<StraightPathFlags>(pathCount);
+		straightPathRefs = gcnew array<PolyReference>(pathCount);
+		for (int i = 0; i < pathCount; i++) {
+			straightPath[i].X = _straightPath[i * 3 + 0];
+			straightPath[i].Y = _straightPath[i * 3 + 1];
+			straightPath[i].Z = _straightPath[i * 3 + 2];
+
+			straightPathFlags[i] = static_cast<StraightPathFlags>(_straightPathFlags[i]);
+			straightPathRefs[i] = _straightPathRefs[i];
+		}
+	}
+
+	delete _sp;
+	delete _ep;
+	delete _straightPath;
+	delete _straightPathFlags;
+	delete _straightPathRefs;
+	return rv;
+}
+
+OperationStatus NavigationMeshQuery::InitializeSlicedFindPath(PolyReference startRef, PolyReference endRef, Vector3 startPos,
+															  Vector3 endPos, QueryFilter^ filter, FindPathOptions options)
+{
+	float* _sp = new float[3];
+	float* _ep = new float[3];
+	_sp[0] = startPos.X; _sp[1] = startPos.Y; _sp[2] = startPos.X;
+	_ep[0] = endPos.X; _ep[1] = endPos.Y; _ep[2] = endPos.X;
+
+	auto rv = static_cast<OperationStatus>(nativeQuery->initSlicedFindPath(startRef, endRef, _sp, _ep, filter->nativeFilter, static_cast<uint>(options)));
+
+	delete _sp;
+	delete _ep;
+	return rv;
+}
+
+OperationStatus Native::Detour::NavigationMeshQuery::UpdateSlicedFindPath(const int maxIter, int % doneIters)
+{
+	int _doneIters;
+	auto rv = static_cast<OperationStatus>(nativeQuery->updateSlicedFindPath(maxIter, &_doneIters));
+	doneIters = _doneIters;
+	return rv;
+}
+
+OperationStatus Native::Detour::NavigationMeshQuery::FinalizeSlicedFindPath(array<PolyReference>^% path, const int maxPath)
+{
+	PolyReference* _path = new PolyReference[maxPath];
+	int pathCount;
+
+	auto rv = static_cast<OperationStatus>(nativeQuery->finalizeSlicedFindPath(_path, &pathCount, maxPath));
+	if (!Detour::StatusFailed(rv)) {
+		path = gcnew array<PolyReference>(pathCount);
+		for (int i = 0; i < pathCount; i++) {
+			path[i] = _path[i];
+		}
+	}
+	delete _path;
+	return rv;
+}
+
+OperationStatus Native::Detour::NavigationMeshQuery::FinalizeSlicedFindPathPartial(array<PolyReference>^ existing, int existingSize, array<PolyReference>^% path, const int maxPath)
+{
+	PolyReference* _path = new PolyReference[maxPath];
+	int pathCount;
+
+	pin_ptr<PolyReference> _existing = &existing[0];
+	auto rv = static_cast<OperationStatus>(nativeQuery->finalizeSlicedFindPathPartial(_existing, existingSize, _path, &pathCount, maxPath));
+	if (!Detour::StatusFailed(rv)) {
+		path = gcnew array<PolyReference>(pathCount);
+		for (int i = 0; i < pathCount; i++) {
+			path[i] = _path[i];
+		}
+	}
 	delete _path;
 	return rv;
 }
--- Native/Native.Detour/NavigationMeshQuery.h
+++ Native/Native.Detour/NavigationMeshQuery.h
@@ -48,6 +48,69 @@ namespace Native {
 				Vector3 startPos, Vector3 endPos, QueryFilter^ filter,[Out] array<PolyReference>^% path, const int maxPath);
 				
 
+			/// Finds the straight path from the start to the end position within the polygon corridor.
+			///  @param[in]		startPos			Path start position. [(x, y, z)]
+			///  @param[in]		endPos				Path end position. [(x, y, z)]
+			///  @param[in]		path				An array of polygon references that represent the path corridor.
+			///  @param[in]		pathSize			The number of polygons in the @p path array.
+			///  @param[out]	straightPath		Points describing the straight path. [(x, y, z) * @p straightPathCount].
+			///  @param[out]	straightPathFlags	Flags describing each point. (See: #dtStraightPathFlags) [opt]
+			///  @param[out]	straightPathRefs	The reference id of the polygon that is being entered at each point. [opt]
+			///  @param[out]	straightPathCount	The number of points in the straight path.
+			///  @param[in]		maxStraightPath		The maximum number of points the straight path arrays can hold.  [Limit: > 0]
+			///  @param[in]		options				Query options. (see: #dtStraightPathOptions)
+			/// @returns The status flags for the query.
+			OperationStatus FindStraightPath(Vector3 startPos, Vector3 endPos, array<PolyReference>^ path, int pathSize,
+				[Out] array<Vector3>^% straightPath, [Out] array<StraightPathFlags>^% straightPathFlags,
+				[Out] array<PolyReference>^% straightPathRefs, int maxStraightPath, StraightPathOptions options);
+
+
+			///@}
+			/// @name Sliced Pathfinding Functions
+			/// Common use case:
+			///	-# Call initSlicedFindPath() to initialize the sliced path query.
+			///	-# Call updateSlicedFindPath() until it returns complete.
+			///	-# Call finalizeSlicedFindPath() to get the path.
+			///@{ 
+
+			/// Intializes a sliced path query.
+			///  @param[in]		startRef	The refrence id of the start polygon.
+			///  @param[in]		endRef		The reference id of the end polygon.
+			///  @param[in]		startPos	A position within the start polygon. [(x, y, z)]
+			///  @param[in]		endPos		A position within the end polygon. [(x, y, z)]
+			///  @param[in]		filter		The polygon filter to apply to the query.
+			///  @param[in]		options		query options (see: #dtFindPathOptions)
+			/// @returns The status flags for the query.
+			OperationStatus InitializeSlicedFindPath(PolyReference startRef, PolyReference endRef,
+				Vector3 startPos, Vector3 endPos, QueryFilter^ filter, FindPathOptions options);
+				
+
+			/// Updates an in-progress sliced path query.
+			///  @param[in]		maxIter		The maximum number of iterations to perform.
+			///  @param[out]	doneIters	The actual number of iterations completed. [opt]
+			/// @returns The status flags for the query.
+			OperationStatus UpdateSlicedFindPath(const int maxIter,[Out] int% doneIters);
+
+			/// Finalizes and returns the results of a sliced path query.
+			///  @param[out]	path		An ordered list of polygon references representing the path. (Start to end.) 
+			///  							[(polyRef) * @p pathCount]
+			///  @param[out]	pathCount	The number of polygons returned in the @p path array.
+			///  @param[in]		maxPath		The max number of polygons the path array can hold. [Limit: >= 1]
+			/// @returns The status flags for the query.
+			OperationStatus FinalizeSlicedFindPath([Out] array<PolyReference>^% path, const int maxPath);
+
+			/// Finalizes and returns the results of an incomplete sliced path query, returning the path to the furthest
+			/// polygon on the existing path that was visited during the search.
+			///  @param[in]		existing		An array of polygon references for the existing path.
+			///  @param[in]		existingSize	The number of polygon in the @p existing array.
+			///  @param[out]	path			An ordered list of polygon references representing the path. (Start to end.) 
+			///  								[(polyRef) * @p pathCount]
+			///  @param[out]	pathCount		The number of polygons returned in the @p path array.
+			///  @param[in]		maxPath			The max number of polygons the @p path array can hold. [Limit: >= 1]
+			/// @returns The status flags for the query.
+			OperationStatus FinalizeSlicedFindPathPartial(array<PolyReference>^ existing, int existingSize,
+				[Out] array<PolyReference>^% path, const int maxPath);
+
 			NavigationMesh^ GetNavigationMesh() {
 				return associatedNavMesh;
 			}
