72041fcadd4ca065b3cd8ccc76cf650cd9c0e470
Vladimir <vvkraf@gmail.com>
2017-03-02 20:23:40 +0300

    US : hf alligment rules

--- Engine/Engine/Graphics/Ubershaders/UbershaderGenerator.cs
+++ Engine/Engine/Graphics/Ubershaders/UbershaderGenerator.cs
@@ -62,7 +62,7 @@ namespace Fusion.Engine.Graphics.Ubershaders {
 						var nestedType	=  (Type)member;
 
                         //	https://msdn.microsoft.com/en-us/library/windows/desktop/bb509632(v=vs.85).aspx
-						//CheckAlligmentRules(nestedType);
+						CheckAlligmentRules(nestedType);
 
 						sb.AppendFormat("// {0}\r\n", nestedType);
 						sb.AppendFormat("// Marshal.SizeOf = {0}\r\n", Marshal.SizeOf(nestedType));
@@ -142,21 +142,11 @@ namespace Fusion.Engine.Graphics.Ubershaders {
             foreach (var fi in fields) {
                 int size = Marshal.SizeOf(fi.FieldType);
                 int curOffset = Marshal.OffsetOf(type, fi.Name).ToInt32();
-                
-                //it means that user uses a FieldOffset attribute.
-                if (curOffset != accOffset) {
 
-                    if (accOffset / 16 == curOffset / 16 && accOffset % 16 != 0 && accOffset % 16 + size >= 16) {
-                        throw new ArgumentException($"Field {fi.Name} in struct {type.Name} has wrong offset {curOffset}. Offset must be {accOffset / 16 * 17}");
-                    }
-
-                } else {
-
-                    if (accOffset % 16 + size >= 16) {
-                        throw new ArgumentException($"Field {fi.Name} in struct {type.Name} has wrong offset {curOffset}. Offset must be {accOffset / 16 * 17}");
-                    }
+                if (accOffset / 16 == curOffset / 16 && accOffset % 16 != 0 && accOffset % 16 + size > 16)
+                {
+                    throw new ArgumentException($"Field {fi.Name} in struct {type.Name} has wrong offset {curOffset}. Offset must be {accOffset / 16 * 17}");
                 }
-
                 accOffset = curOffset + size;
             }
         }
